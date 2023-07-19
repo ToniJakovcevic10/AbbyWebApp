336a87c495bdd6223ae8130f300ced2403967089
Toni Jakovcevic <toni.jakovcevic10@gmail.com>
2023-07-19 14:08:49 +0200

    push project to git

---
+++ Model/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]   //odreduje primary key, ako nema ovog, onda Id automatski postavlja,
+                //ostalo ne moze
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name="Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be in range 1-100!!!")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
